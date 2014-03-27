dc1df454d014f7702314be6936305ecae68f7b91
drumber-1 <drumber01@gmail.com>
2014-03-27 20:31:18 +0000

    Update android manifest to reflect package name changes

--- NestedTicTacToeAndroid/gen/net/drumber/nestedtictactoe/BuildConfig.java
+++ NestedTicTacToeAndroid/gen/net/drumber/nestedtictactoe/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package close.nestedtictactoe;
+package net.drumber.nestedtictactoe;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
