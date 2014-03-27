006cbf0a6b121feb5433c58e69bfc8896f1a2907
drumber-1 <drumber01@gmail.com>
2014-03-27 19:48:33 +0000

    Increased default desktop resolution

--- NestedTicTacToeDesktop/src/close/nestedtictactoe/NestedTicTacToeDesktop.java
+++ NestedTicTacToeDesktop/src/close/nestedtictactoe/NestedTicTacToeDesktop.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
 public class NestedTicTacToeDesktop {
 	public static void main(String[] args) {
-		new LwjglApplication(new NestedTicTacToe(), "Nested Tic Tac Toe", 640, 360, true);
+		new LwjglApplication(new NestedTicTacToe(), "Nested Tic Tac Toe", 1280, 720, true);
 	}
 
 }
